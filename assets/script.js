2f92f9cf89a61553bfc2c6121a598f8aa6de7b9a
Anaê Ross <anae08@gmail.com>
2023-08-10 20:52:24 -0300

    Adicionando conteúdo do curso Discover da RocketSeat

---
+++ assets/script.js
@@ -0,0 +1,25 @@
+//declaro e atribuo valor a variável
+let nome = "Anaê"
+let boasVindas = "Boas Vindas " + nome
+
+//constantes não podem mudar de valor
+const a = true
+const b = false
+const serHumano = true
+serHumano = false //Error
+
+//Functions
+
+
+function chamarAlerta(text) {
+    return text
+}
+
+chamarAlerta("Anaê")
+
+//Objetos
+
+const carro = {
+    cor: "preto",
+    ano: "19988"
+}
